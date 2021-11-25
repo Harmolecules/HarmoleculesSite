67d44407f7ab5455f1c230292cf8ea2bd01260c1
HARMOLECULES <94574662+Harmolecules@users.noreply.github.com>
2021-11-24 22:29:25 -0600

    Add files via upload

--- gallery.js
+++ gallery.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('#btn-search').click(function() {
         var databack = $("#exampleFormControlTextarea1").val().trim();
-        window.location = ("https://the-marketplace.one/assets/0xb44778bfe8a6c3468dcf7219fb104ba7e42f8d2a/") + (databack)  ;
+        window.open = ("https://the-marketplace.one/assets/0xb44778bfe8a6c3468dcf7219fb104ba7e42f8d2a/") + (databack)  ;
  });
 });
 
